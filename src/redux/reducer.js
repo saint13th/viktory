65967f5b2345a79d374ed29eacd99d52afd87603
a123 <dmitriy_rodionoff@mail.ru>
2018-08-01 21:39:38 +0300

    Updates

--- src/redux/reducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-import {routerReducer as router} from 'react-router-redux'
-import {reducer as form} from 'redux-form'
-import gameReducer, {moduleName as gameModule} from '../ducks/game'
-
-export default combineReducers({
-    router, form,
-    [gameModule]: gameReducer,
-})
